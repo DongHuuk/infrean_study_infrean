ceadaf727f366ccf4931367b83ce94e0fa630563
kuron <drivespublic@gmail.com>
2020-05-02 21:02:41 +0900

    non error

--- src/main/java/com/infrean_study/module/event/event/EnrollmentEvent.java
+++ src/main/java/com/infrean_study/module/event/event/EnrollmentEvent.java
@@ -3,7 +3,8 @@ package com.infrean_study.module.event.event;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Enrollment {
+public
+class Enrollment {
 
 
 
--- src/main/java/com/infrean_study/module/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/infrean_study/module/study/StudyRepositoryExtensionImpl.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface StudyRepositoryExtension {
 
-    List<Study> findByKeywork(String keywork);
+    List<com.infrean_study.domain.Study> findByKeywork(String keywork);
 
 }
